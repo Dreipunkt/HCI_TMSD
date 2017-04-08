096d771a77e4d7d2f57f4490a04f293ad80072ca
Christian Rauch <crauc3@gmail.com>
2017-04-08 19:05:06 +0200

    first activity

---
+++ app/src/main/java/univie/g02/t06/tmsd/MainActivity.java
@@ -0,0 +1,13 @@
+package univie.g02.t06.tmsd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
