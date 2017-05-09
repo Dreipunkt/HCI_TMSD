c027ec164be3a21e50a47d233d95276f6cf60e01
Christian Rauch <crauc3@gmail.com>
2017-05-09 19:55:42 +0200

    hdf-java library + dataset package

---
+++ app/src/main/java/univie/g02/t06/tmsd/dataset/DatasetAction.java
@@ -0,0 +1,17 @@
+package univie.g02.t06.tmsd.dataset;
+
+public class DatasetAction {
+
+    public DatasetAction() {
+
+    }
+
+    public Song[] getSongsByTitle(String title) {
+
+    }
+
+    public Song[] getSongsByArtist(String artist) {
+
+    }
+
+}
---
+++ app/src/main/java/univie/g02/t06/tmsd/dataset/Song.java
@@ -0,0 +1,31 @@
+package univie.g02.t06.tmsd.dataset;
+
+public class Song {
+    private String title;
+    private String artist;
+    private double duration; // in seconds
+    private int year;
+
+    Song(String title, String artist, double duration, int year) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
